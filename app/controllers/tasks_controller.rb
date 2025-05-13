e2073ffa53a66c200e37955bf10f3de622c6d6a0
Kaho Maeda <kaho@tutamail.com>
2025-05-13 22:42:58 +0900

    icons added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,12 +21,17 @@ class TasksController < ApplicationController
     end
   end
 
-  def task_params
-    params.require(:task).permit(:title, :details)
-  end
-
   def edit
     @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render "edit", status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -34,4 +39,11 @@ class TasksController < ApplicationController
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
